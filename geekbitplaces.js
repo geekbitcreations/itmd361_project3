0898cb241e38708e9ae9aec26da904cb2005d531
geekbitcreations <dbarndt@hawk.iit.edu>
2017-11-13 16:48:55 -0600

    created another javascript file with a basic test google api
    I decided to make a simple test javascript with a google api, and then slowly add more script to the previous file to try and see what went wrong.

--- geekbitplaces.js
+++ geekbitplaces.js
@@ -1,14 +1,17 @@
-function initMap()
+/*function initMap()
 {
   //var geek = document.getElementById('map');
   //var geekbitplaces = new google.maps.LatLng(42.3704366,-87.938236);
   //var geekplaces = {lat: 42.3704366, lng: -87.938236}
   
   var geekplaces1 = {lat: 42.3704366, lng: -87.938236}; // Six Flags Great America
-  var geekplaces2 = {lat: 42.4963589, lng: -87.9583877}; // Bristol Renaissance Faire
-  var geekplaces3 = {lat: 41.7905766, lng: -87.5852546}; // Museum of Science and Industry
-  var geekplaces4 = {lat: 41.8839693, lng: -87.632672}; // Christkindlmarket
-  var geekplaces5 = {lat: 41.8916244, lng: -87.6116684}; // Navy Pier
+  //var geekplaces2 = {lat: 42.4963589, lng: -87.9583877}; // Bristol Renaissance Faire
+  //var geekplaces3 = {lat: 41.7905766, lng: -87.5852546}; // Museum of Science and Industry
+  //var geekplaces4 = {lat: 41.8839693, lng: -87.632672}; // Christkindlmarket
+  //var geekplaces5 = {lat: 41.8916244, lng: -87.6116684}; // Navy Pier
+  
+  //var geekMap = new google.maps.Map(geek, mapOptions);
+  var map = new google.maps.Map(document.getElementById('map', mapOptions));
   
   var mapOptions = {
     //center: new google.maps.LatLng(42.3704366,-87.938236);,
@@ -20,19 +23,16 @@ function initMap()
     }
   };
 
-  //var geekMap = new google.maps.Map(geek, mapOptions);
-  var map = new google.maps.Map(document.getElementById('map', mapOptions));
-
   var markerOptions = new google.maps.Marker({
     position: geekplaces1,
     //position: new google.maps.LatLng(42.3704366,-87.938236)
     map: map,
-    animation: google.maps.Animation.BOUNCE,
+    animation: google.maps.Animation.DROP,
     //icon: 'geekbiticon.png'
   });
     
-  var marker = new google.maps.Marker(markerOptions);
-  marker.setMap(map);
+  //var marker = new google.maps.Marker(markerOptions);
+  //marker.setMap(map);
 
   //var infostring = '<h1>Six Flags Great America</h1><p>Best places here are Hurricane Harbor, the Whizzer, American Eagle, the Viper, Dark Knight, and Justice League.</p>';
 
@@ -48,7 +48,7 @@ function initMap()
   });
 }
 
-google.maps.event.addDomListener(window, 'load', initMap);
+google.maps.event.addDomListener(window, 'load', initMap);*/
 
 
 /*<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDYsouagwj_KvlqIJ8yFFGv2iV21YOm1-I&callback=initMap"
@@ -56,3 +56,37 @@ google.maps.event.addDomListener(window, 'load', initMap);
   
 /*<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDYsouagwj_KvlqIJ8yFFGv2iV21YOm1-I&callback=initMap"
     async defer></script>*/
+
+var geekplaces1 = {lat: 42.3704366, lng: -87.938236}; // Six Flags Great America
+var geekplaces2 = {lat: 42.4963589, lng: -87.9583877}; // Bristol Renaissance Faire
+var geekplaces3 = {lat: 41.7905766, lng: -87.5852546}; // Museum of Science and Industry
+var geekplaces4 = {lat: 41.8839693, lng: -87.632672}; // Christkindlmarket
+var geekplaces5 = {lat: 41.8916244, lng: -87.6116684}; // Navy Pier
+    
+var marker;
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: {lat: 42.3704366, lng: -87.938236}
+  });
+
+  marker = new google.maps.Marker({
+    map: map,
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    position: {lat: 42.3704366, lng: -87.938236}
+    icon: '/images/geekbiticon.png'
+  });
+  marker.addListener('click', toggleBounce);
+}
+
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } 
+  
+  else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
