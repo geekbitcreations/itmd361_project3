ad0956c0e09d18a6387af4371f90a637a13a37f0
geekbitcreations <dbarndt@hawk.iit.edu>
2017-11-13 17:12:23 -0600

    still trying to fix the bugs on my original javascript

--- geekbitplaces.js
+++ geekbitplaces.js
@@ -1,16 +1,16 @@
-/*function initMap()
+function init()
 {
-  //var geek = document.getElementById('map');
-  //var geekbitplaces = new google.maps.LatLng(42.3704366,-87.938236);
-  //var geekplaces = {lat: 42.3704366, lng: -87.938236}
+  var geek = document.getElementById('map');
+  var geekbitplaces = new google.maps.LatLng(42.3704366,-87.938236);
+  var geekplaces = {lat: 42.3704366, lng: -87.938236}
   
   var geekplaces1 = {lat: 42.3704366, lng: -87.938236}; // Six Flags Great America
-  //var geekplaces2 = {lat: 42.4963589, lng: -87.9583877}; // Bristol Renaissance Faire
-  //var geekplaces3 = {lat: 41.7905766, lng: -87.5852546}; // Museum of Science and Industry
-  //var geekplaces4 = {lat: 41.8839693, lng: -87.632672}; // Christkindlmarket
-  //var geekplaces5 = {lat: 41.8916244, lng: -87.6116684}; // Navy Pier
+  var geekplaces2 = {lat: 42.4963589, lng: -87.9583877}; // Bristol Renaissance Faire
+  var geekplaces3 = {lat: 41.7905766, lng: -87.5852546}; // Museum of Science and Industry
+  var geekplaces4 = {lat: 41.8839693, lng: -87.632672}; // Christkindlmarket
+  var geekplaces5 = {lat: 41.8916244, lng: -87.6116684}; // Navy Pier
   
-  //var geekMap = new google.maps.Map(geek, mapOptions);
+  var geekMap = new google.maps.Map(geek, mapOptions);
   var map = new google.maps.Map(document.getElementById('map', mapOptions));
   
   var mapOptions = {
@@ -25,20 +25,19 @@
 
   var markerOptions = new google.maps.Marker({
     position: geekplaces1,
-    //position: new google.maps.LatLng(42.3704366,-87.938236)
+    position: new google.maps.LatLng(42.3704366,-87.938236)
     map: map,
     animation: google.maps.Animation.DROP,
-    //icon: 'geekbiticon.png'
+    icon: '/images/geekbiticon.png'
   });
     
-  //var marker = new google.maps.Marker(markerOptions);
-  //marker.setMap(map);
+  var marker = new google.maps.Marker(markerOptions);
+  marker.setMap(map);
 
-  //var infostring = '<h1>Six Flags Great America</h1><p>Best places here are Hurricane Harbor, the Whizzer, American Eagle, the Viper, Dark Knight, and Justice League.</p>';
+  var infostring = '<h1>Six Flags Great America</h1><p>Best places here are Hurricane Harbor, the Whizzer, American Eagle, the Viper, Dark Knight, and Justice League.</p>';
 
   var infoWindowOptions = new google.maps.InfoWindow({
-    content: '<h1>Six Flags Great America</h1>
-    <p>Best places here are Hurricane Harbor, the Whizzer, American Eagle, the Viper, Dark Knight, and Justice League.</p>'
+    content: '<h1>Six Flags Great America</h1><p>Best places here are Hurricane Harbor, the Whizzer, American Eagle, the Viper, Dark Knight, and Justice League.</p>'
   });
 
   var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
@@ -48,7 +47,7 @@
   });
 }
 
-google.maps.event.addDomListener(window, 'load', initMap);*/
+google.maps.event.addDomListener(window, 'load', init);
 
 
 /*<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDYsouagwj_KvlqIJ8yFFGv2iV21YOm1-I&callback=initMap"
@@ -57,11 +56,19 @@ google.maps.event.addDomListener(window, 'load', initMap);*/
 /*<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDYsouagwj_KvlqIJ8yFFGv2iV21YOm1-I&callback=initMap"
     async defer></script>*/
 
-var geekplaces1 = {lat: 42.3704366, lng: -87.938236}; // Six Flags Great America
+/*var geekplaces1 = {lat: 42.3704366, lng: -87.938236}; // Six Flags Great America
 var geekplaces2 = {lat: 42.4963589, lng: -87.9583877}; // Bristol Renaissance Faire
 var geekplaces3 = {lat: 41.7905766, lng: -87.5852546}; // Museum of Science and Industry
 var geekplaces4 = {lat: 41.8839693, lng: -87.632672}; // Christkindlmarket
 var geekplaces5 = {lat: 41.8916244, lng: -87.6116684}; // Navy Pier
+
+var geekplaces = [
+  ['Six Flags Great America', lat: 42.3704366, lng: -87.938236, 1]
+  ['Bristol Renaissance', lat: 42.3704366, lng: -87.938236, 1]
+  ['Six Flags Great America', lat: 42.3704366, lng: -87.938236, 1]
+  ['Six Flags Great America', lat: 42.3704366, lng: -87.938236, 1]
+  ['Six Flags Great America', lat: 42.3704366, lng: -87.938236, 1]
+  
     
 var marker;
 
@@ -76,7 +83,7 @@ function initMap() {
     draggable: true,
     animation: google.maps.Animation.DROP,
     position: {lat: 42.3704366, lng: -87.938236}
-    icon: '/images/geekbiticon.png'
+    //icon: '/images/geekbiticon.png'
   });
   marker.addListener('click', toggleBounce);
 }
@@ -89,4 +96,4 @@ function toggleBounce() {
   else {
     marker.setAnimation(google.maps.Animation.BOUNCE);
   }
-}
+}*/
