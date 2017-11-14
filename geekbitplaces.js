49c4363b124102593f6993bc0c630ec07efdd4d5
geekbitcreations <dbarndt@hawk.iit.edu>
2017-11-14 09:28:22 -0600

    cleaning up code and trying to get the other locations
    trying to get all the other locations to load, but now the map is broken again

--- geekbitplaces.js
+++ geekbitplaces.js
@@ -1,69 +1,47 @@
-/*function initMap()
-{
-  var geek = document.getElementById('map');
-  var geekbitplaces = new google.maps.LatLng(42.3704366,-87.938236);
-  var geekplaces = {lat: 42.3704366, lng: -87.938236}
-  
-  var geekplaces1 = {lat: 42.3704366, lng: -87.938236}; // Six Flags Great America
-  var geekplaces2 = {lat: 42.4963589, lng: -87.9583877}; // Bristol Renaissance Faire
-  var geekplaces3 = {lat: 41.7905766, lng: -87.5852546}; // Museum of Science and Industry
-  var geekplaces4 = {lat: 41.8839693, lng: -87.632672}; // Christkindlmarket
-  var geekplaces5 = {lat: 41.8916244, lng: -87.6116684}; // Navy Pier
-  
-  var geekMap = new google.maps.Map(geek, mapOptions);
-  var map = new google.maps.Map(document.getElementById('map', mapOptions));
-  
-  var mapOptions = {
-    //center: new google.maps.LatLng(42.3704366,-87.938236);,
-    center: geekplaces1,
-    zoom: 12,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    mapTypeControlOptions: {
-      position: google.maps.ControlPosition.BOTTOM_CENTER
-    }
+function initMap() {
+  // Six Flags Great America
+  var geekplaces1 = {
+    info: '<strong><h1>Six Flags Great America</h1></strong><p>Favorite rides: Hurricane Harbor, Whizzer, American Eagle, Viper, Dark Knight, and Justice League: Battle for Metropolis.</p>',
+    lat: 42.372559,
+    lng: -87.938236
+  }; 
+
+  // Bristol Renaissance Faire
+  var geekplaces2 = {
+    info: '<strong><h1>Brisol Renaissance Faire</h1></strong><p>Favorite places: Jousting, Mud Show, Barely Balanced, The Swordsmen, Moonie, great food, costumes, and The Fantasticals.</p>',
+    lat: 42.4963589, 
+    lng: -87.9583877
+  }; 
+
+  // Museum of Science and Industry
+  var geekplaces3 = {
+    info: '<strong><h1>Museum of Science and Industry</h1></strong><p>Favorite places: Tornado, Coal Mine, Fairy Castle, Trees Around the World, Revolution, Jurassic World, and Toy Master.</p>',
+    lat: 41.7905766, 
+    lng: -87.5852546
   };
 
-  var markerOptions = new google.maps.Marker({
-    position: geekplaces1,
-    position: new google.maps.LatLng(42.3704366,-87.938236)
-    map: map,
-    animation: google.maps.Animation.DROP,
-    icon: 'geekbiticon.png'
-  });
-    
-  var marker = new google.maps.Marker(markerOptions);
-  marker.setMap(map);
+  // Christkindlmarket
+  var geekplaces4 = {
+    info: '<strong><h1>Christkindlmarket</h1></strong><p>Favorite places: shopping, the great food, hot cider, ornaments, and caroling.</p>',
+    lat: 41.8839693, 
+    lng: -87.632672
+  }; 
 
-  var infostring = '<h1>Six Flags Great America</h1><p>Best places here are Hurricane Harbor, the Whizzer, American Eagle, the Viper, Dark Knight, and Justice League.</p>';
+  // Navy Pier
+  var geekplaces5 = {
+    info: '<strong><h1>Navy Pier/h1></strong><p>Favorite places: Winter Wonderfest, ferris wheel, and the garden.</p>',
+    lat: 41.8916244, 
+    lng: -87.6116684
+  }; 
 
-  var infoWindowOptions = new google.maps.InfoWindow({
-    content: '<h1>Six Flags Great America</h1><p>Best places here are Hurricane Harbor, the Whizzer, American Eagle, the Viper, Dark Knight, and Justice League.</p>'
-  });
-
-  var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
-    
-  google.maps.event.addListener(marker, 'click', function(e) {
-    infoWindow.open(map, marker);
-  });
-}
-
-google.maps.event.addDomListener(window, 'load', init);*/
-
-
-/*<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDYsouagwj_KvlqIJ8yFFGv2iV21YOm1-I&callback=initMap"
-  type="text/javascript"></script>*/
+  var locations = [
+    [geekplaces1.info, geekplaces1.lat, geekplaces1.lng, 0],
+    [geekplaces2.info, geekplaces2.lat, geekplaces2.lng, 1],
+    [geekplaces3.info, geekplaces3.lat, geekplaces3.lng, 2],
+    [geekplaces4.info, geekplaces4.lat, geekplaces4.lng, 3],
+    [geekplaces5.info, geekplaces5.lat, geekplaces5.lng, 4],
+  ];
   
-/*<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDYsouagwj_KvlqIJ8yFFGv2iV21YOm1-I&callback=initMap"
-    async defer></script>*/
-
-/*var geekplaces1 = {lat: 42.372559, lng: -87.938236}; // Six Flags Great America
-var geekplaces2 = {lat: 42.4963589, lng: -87.9583877}; // Bristol Renaissance Faire
-var geekplaces3 = {lat: 41.7905766, lng: -87.5852546}; // Museum of Science and Industry
-var geekplaces4 = {lat: 41.8839693, lng: -87.632672}; // Christkindlmarket
-var geekplaces5 = {lat: 41.8916244, lng: -87.6116684}; // Navy Pier
-*/
-
-function initMap() {
   var map = new google.maps.Map(document.getElementById('map', mapOptions), {
     zoom: 12,
     center: {lat: 42.372559, lng: -87.938236}
@@ -87,11 +65,26 @@ function initMap() {
     }
   };
     
-  var infoString = '<h1>Six Flags Great America</h1><p>Favorite rides: Hurricane Harbor, Whizzer, American Eagle, Viper, Dark Knight, and Justice League: Battle for Metropolis.</p>';
+  /*var infoString = '<h1>Six Flags Great America</h1><p>Favorite rides: Hurricane Harbor, Whizzer, American Eagle, Viper, Dark Knight, and Justice League: Battle for Metropolis.</p>';
 
   var infoWindow = new google.maps.InfoWindow({
       content: infoString
-  	});
+  	});*/
+    
+  var infoWindow = new google.maps.InfoWindow({});
+  
+  var placeMarker, i;
+  
+  for (i = 0; i < locations.length; i++) {
+    placeMarker = new google.maps.Marker({
+      position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+      map: map
+    });
+    
+    /*google.maps.event.addListener(placeMarker, 'click', (function (placeMarker, i) {
+        infoWindow.setContent(locations[i][0]);
+        infoWindow.open(map, placeMarker);
+      )};*/
     
   google.maps.event.addListener(placeMarker, 'mouseover', function(e) {
     infoWindow.open(map, placeMarker);
@@ -108,11 +101,11 @@ function initMap() {
 }
 
 function toggleBounce() {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
+  if (placeMarker.getAnimation() !== null) {
+    placeMarker.setAnimation(null);
   } 
   
   else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
+    placeMarker.setAnimation(google.maps.Animation.BOUNCE);
   }
 }
